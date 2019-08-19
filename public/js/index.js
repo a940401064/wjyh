d5aa66ef4d739c03f4b1a5f54bbbdef14e1e476c
ali0o0 <940401064@qq.com>
2019-08-19 20:02:57 +0800

    8-19添加了jq的js交互效果

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,19 @@
 $(function(){
     $("#byzq").on("click","li",function(){
-        $(this).children(":first-child").addClass("gift-first").parent().siblings().children(":first-child").removeClass("gift-first");
+        var $li=$(this);
+        $li.children(":first-child").addClass("gift-first").parent().siblings().children(":first-child").removeClass("gift-first");
         
-        var i=parseInt($(this).attr("data-byzq"))
+        var i=parseInt($li.attr("data-byzq"))
        
         $(`#gift${i}`).addClass("show").siblings().removeClass("show");
-
-
     })
 
 
-
-
-
-
-
-
-
-
-
+    $("#gn-fr-dl").on("click","a",function(){
+        var $a=$(this);
+        $a.addClass("gonggao").siblings().removeClass("gonggao");
+        var i=parseInt($a.attr("data-gonggao"));
+        $(`#gn-fr-dl-${i}`).addClass("show").siblings().removeClass("show")
+    })
 
 })
